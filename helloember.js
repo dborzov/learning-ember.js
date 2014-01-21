7ffc85c113aba82f26271a50428b0a1258f7b089
Peter Borzov <tihoutrom@gmail.com>
2014-01-21 15:07:45 -0500

    names fix

--- helloember.js
+++ helloember.js
@@ -1,4 +1,40 @@
-window.Todos = Ember.Application.create();
-Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
+window.MyApp = Ember.Application.create();
+MyApp.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+// setting up router
+MyApp.Router.map(function() {
+  this.resource('LandingPageTemplate',{path:'/'});
+  this.resource('TableTemplate',{path:'/table'})
+});
+
+
+MyApp.VendorModel = DS.Model.extend({
+	companyName: DS.attr('string'),
+	Approved: DS.attr('boolean'),
+	contactEmail: DS.attr('string')
+});
+
+
+MyApp.VendorModel.FIXTURES = [
+    {
+        id: 1,
+        companyName: 'Glove makers',
+        Approved: true,
+        contactEmail: 'ceo@glovemakers.com'
+    },
+    {
+    	id: 2,
+    	title: 'Colonial Floss Inc',
+    	Approved: false,
+    	contactEmail: 'info@colonialfloss.com'
+    },
+    {
+    	id:3,
+    	title: 'Executive Toothbrushes, LLC',
+    	Approved: true,
+    	contactEmail: 'peter.borzov@gmail.com'
+    }
+];
+
 
 
